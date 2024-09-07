a837503aa73e385ed5c9a54f24d5e491dfdcdc84
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-07 16:32:55 +0100

    dashboard navbar

--- components/mainNav.tsx
+++ components/mainNav.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 import { useParams, usePathname } from 'next/navigation';
 
 const MainNav = ({
@@ -9,8 +10,29 @@ const MainNav = ({
 }: React.HTMLAttributes<HTMLElement>) => {
   const pathname = usePathname();
   const params = useParams();
-  const routes = [{}];
-  return <nav className={cn('flex items-center space-x-4 lg:space-x-6')}></nav>;
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: 'Settings',
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+  return (
+    <nav className={cn('flex items-center space-x-4 lg:space-x-6')}>
+      {routes.map((route) => (
+        <Link
+          className={cn(
+            'text-sm font-medium transition-colors hover:text-primary',
+            route.active ? 'text-black dark:text-white' : 'text-muted '
+          )}
+          href={route.href}
+          key={route.href}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default MainNav;
