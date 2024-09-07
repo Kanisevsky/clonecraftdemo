1e66cfeeea7f839d14209cf98ed41dbb214d9210
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-07 17:39:17 +0100

    dashboard navbar

--- components/storeSwitcher.tsx
+++ components/storeSwitcher.tsx
@@ -76,7 +76,7 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
                   className="text-sm"
                 >
                   <StoreIcon className="mr-2 h-4 w-4" />
-                  {store.label}
+                  {currentStore?.label}
                   <Check
                     className={cn(
                       'ml-auto h-4 w-4',
