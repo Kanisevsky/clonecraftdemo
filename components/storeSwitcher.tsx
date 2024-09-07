8eac1f2136f1d98a4a1e814237eb43938edb407a
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-07 16:38:42 +0100

    dashboard navbar

---
+++ components/storeSwitcher.tsx
@@ -0,0 +1,11 @@
+import { PopoverTrigger } from './ui/popover';
+
+type PopoverTriggerProps = React.ComponentPropsWithRef<typeof PopoverTrigger>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {}
+
+const StoreSwitcher = () => {
+  return <div>StreSwitcher</div>;
+};
+
+export default StoreSwitcher;
