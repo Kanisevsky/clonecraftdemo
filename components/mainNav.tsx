e98c68a151fffc7201f936f888969d85e66f97b8
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-07 16:18:47 +0100

    dashboard navbar

---
+++ components/mainNav.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+const mainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  return <p>Mian Navigation</p>;
+};
+
+export default mainNav;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,15 @@
+import { UserButton } from '@clerk/nextjs';
+import MainNav from './mainNav';
+
 const navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <div className="">This will be a store switcher</div>
-        <div className="This will be the routes"></div>
+        <MainNav />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton />
+        </div>
       </div>
     </div>
   );
