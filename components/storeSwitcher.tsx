3ec2196d5ba1f4fc0be15697e2d099dc381f23f9
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-07 16:42:42 +0100

    dashboard navbar

--- components/storeSwitcher.tsx
+++ components/storeSwitcher.tsx
@@ -1,10 +1,24 @@
+'use client';
+
+import { Store } from '@prisma/client';
 import { PopoverTrigger } from './ui/popover';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useParams, useRouter } from 'next/navigation';
 
 type PopoverTriggerProps = React.ComponentPropsWithRef<typeof PopoverTrigger>;
 
-interface StoreSwitcherProps extends PopoverTriggerProps {}
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
 
-const StoreSwitcher = () => {
+const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
+  const storeModal = useStoreModal;
+  const params = useParams();
+  const router = useRouter();
+  const formatedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
   return <div>StreSwitcher</div>;
 };
 
