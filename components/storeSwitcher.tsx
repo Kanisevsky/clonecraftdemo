e799c9628799df98931a6887da1e137720fe5770
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-08 19:20:25 +0100

    settings page

--- components/storeSwitcher.tsx
+++ components/storeSwitcher.tsx
@@ -56,10 +56,10 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
           role="combobox"
           aria-expanded={open}
           aria-label="Select a store"
-          className={cn('w-[200px] justify-between', className)}
+          className={cn('w-[250px] justify-between', className)}
         >
           <IconStore className="mr-2 h-4 w-4" />
-          Current Store
+          {currentStore?.label ? currentStore.label : 'Select a store'}
           <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
