a9e95ccd631b8b8a5910b2b11732c6cf7da8f92d
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-08 17:47:03 +0100

    settings page

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+'use client';
 
-const SettingsForm = () => {
-  return <div></div>;
+import { Store } from '@prisma/client';
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  return <div>Settings Form</div>;
 };
 
 export default SettingsForm;
