ffad2d571cc2df8bb0b4f9156035399bd6bf10f9
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-09 19:57:29 +0100

    Billboard Enitity

--- components/ui/imageUpload.tsx
+++ components/ui/imageUpload.tsx
@@ -1,5 +1,30 @@
-const ImageUpload = () => {
-  return <div></div>;
+'use client';
+
+import { useEffect, useState } from 'react';
+
+interface ImageUploadProps {
+  disables?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  value,
+  disables,
+  onChange,
+  onRemove,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <div>ImageUpload</div>;
 };
 
 export default ImageUpload;
