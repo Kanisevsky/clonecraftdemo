093a23f0e0a8d1d4d52efc5a8f3dde6e0b032250
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-10 14:54:55 +0100

    Billboard Enitity

--- app/api/stores/[storeId]/billboards/route.ts
+++ app/api/stores/[storeId]/billboards/route.ts
@@ -13,7 +13,7 @@ export async function POST(
     const { label, imageUrl } = body;
 
     if (!userId) {
-      return new NextResponse('Unauthorized', { status: 401 });
+      return new NextResponse('Unauthenticated', { status: 401 });
     }
     if (!label) {
       return new NextResponse('Label is Required', { status: 400 });
@@ -30,12 +30,36 @@ export async function POST(
       where: { id: params.storeId, userId },
     });
 
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorised', { status: 403 });
+    }
+
     const billboard = await prismadb.billboard.create({
       data: { label, imageUrl, storeId: params.storeId },
     });
     return NextResponse.json(billboard);
   } catch (error) {
     console.log('[Billboard_POST]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse('Store Id is Required', { status: 400 });
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where: { storeId: params.storeId },
+    });
+
+    return NextResponse.json(billboards);
+  } catch (error) {
+    console.log('[Billboard_GET]', error);
     return new NextResponse('Internal error', { status: 500 });
   }
 }
