26a877894b9f6a96fcfd678d4ba847d09019f31a
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-10 16:11:57 +0100

    Billboard Enitity

--- app/api/stores/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/stores/[storeId]/billboards/[billboardId]/route.ts
@@ -47,3 +47,27 @@ export async function PATCH(
     return new NextResponse('Internal error', { status: 500 });
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse('BillboardId is required', { status: 400 });
+    }
+    const store = await prismadb.store.deleteMany({
+      where: { id: params.storeId, userId },
+    });
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORE_DELETE]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
