d33e128b99d0c303112c434bc451220e2896e5a2
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-11 18:33:38 +0100

    Data table

--- components/ui/apiList.tsx
+++ components/ui/apiList.tsx
@@ -20,6 +20,26 @@ const apiList: React.FC<ApiListProps> = ({ entitiyIdName, entityName }) => {
         description={`${baseUrl}/${entityName}`}
         variant="public"
       />
+      <ApiAlert
+        title="GET"
+        description={`${baseUrl}/${entityName}/{${entitiyIdName}}`}
+        variant="public"
+      />
+      <ApiAlert
+        title="POST"
+        description={`${baseUrl}/${entityName}`}
+        variant="admin"
+      />
+      <ApiAlert
+        title="PATCH"
+        description={`${baseUrl}/${entityName}/{${entitiyIdName}}`}
+        variant="admin"
+      />
+      <ApiAlert
+        title="DELETE"
+        description={`${baseUrl}/${entityName}/{${entitiyIdName}}`}
+        variant="admin"
+      />
     </div>
   );
 };
