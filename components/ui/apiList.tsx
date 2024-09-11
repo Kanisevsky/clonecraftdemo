01a236860bbf45405b1ba3536cc9e815b91659bc
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-11 18:30:28 +0100

    Data table

--- components/ui/apiList.tsx
+++ components/ui/apiList.tsx
@@ -1,6 +1,27 @@
 'use client';
-const apiList = () => {
-  return <div>ApiList</div>;
+
+import { useOrigin } from '@/hooks/use-origin';
+import { useParams } from 'next/navigation';
+import ApiAlert from './apiAlert';
+
+interface ApiListProps {
+  entityName: string;
+  entitiyIdName: string;
+}
+
+const apiList: React.FC<ApiListProps> = ({ entitiyIdName, entityName }) => {
+  const params = useParams();
+  const origin = useOrigin();
+  const baseUrl = `${origin}/api/${params.storeId}`;
+  return (
+    <div>
+      <ApiAlert
+        title="GET"
+        description={`${baseUrl}/${entityName}`}
+        variant="public"
+      />
+    </div>
+  );
 };
 
 export default apiList;
