d966db27af1019fae0b0f6096c71b6aaa1a2ea9a
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-11 17:22:26 +0100

    Data table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cellActions.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cellActions.tsx
@@ -1,13 +1,51 @@
 'use client';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { BillboardColumn } from './columns';
+import { Button } from '@/components/ui/button';
+import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react';
+import toast from 'react-hot-toast';
 
 interface CellActionsProps {
   data: BillboardColumn;
 }
 
-const CellActions: React.FC<CellActionsProps> = () => {
-  return <div>Actions</div>;
+const CellActions: React.FC<CellActionsProps> = ({ data }) => {
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success('Billboard ID copied to the billboard');
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open Menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => onCopy(data.id)}>
+          <Copy className="h-4 w-5 mr-2" />
+          Copy ID
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Edit className="h-4 w-5 mr-2" />
+          Update
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className="h-4 w-5 mr-2" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default CellActions;
