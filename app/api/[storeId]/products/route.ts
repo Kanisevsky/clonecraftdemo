702c94290b097e1616a116fcaf7b5fe49660f58a
Kanisevsky <meltsmokestudio@gmail.com>
2024-09-23 19:29:51 +0100

    Product entity

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -95,17 +95,39 @@ export async function GET(
   { params }: { params: { storeId: string } }
 ) {
   try {
+    const { searchParams } = new URL(req.url);
+    const categoryId = searchParams.get('categoryId') || undefined;
+    const colourId = searchParams.get('colourId') || undefined;
+    const sizeId = searchParams.get('sizeId') || undefined;
+    const isFeatured = searchParams.get('isFeatured');
+
     if (!params.storeId) {
       return new NextResponse('Store Id is Required', { status: 400 });
     }
 
-    const billboards = await prismadb.billboard.findMany({
-      where: { storeId: params.storeId },
+    const products = await prismadb.product.findMany({
+      where: {
+        storeId: params.storeId,
+        categoryId,
+        colourId,
+        sizeId,
+        isFeatured: isFeatured ? true : false,
+        isArchived: false,
+      },
+      include: {
+        images: true,
+        category: true,
+        size: true,
+        colour: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
     });
 
-    return NextResponse.json(billboards);
+    return NextResponse.json(products);
   } catch (error) {
-    console.log('[Billboard_GET]', error);
+    console.log('[PRODUCT_GET]', error);
     return new NextResponse('Internal error', { status: 500 });
   }
 }
